a4659a3382cddd44df5b734b7ee30a6163a5af45
Vittossn <lexymdz@gmail.com>
2024-11-03 03:02:25 -0300

    select your sheet

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,46 @@
 <script setup>
 import * as XLSX from 'xlsx'
 import { ref } from 'vue';
+import ChooseSheet from './components/ChooseSheet.vue';
 
 const excelData = ref([]);
+const selectedSheet = ref([])
+const sheetNames = ref([])
+let workbook = null; 
 
 const handleFileUpload = (e) => {
+
   const file = e.target.files[0]
   if(!file) return;
 
   const reader = new FileReader();
+
   reader.onload = (e) => {
     const data = new Uint8Array(e.target.result);
-    const workbook = XLSX.read(data, { type: 'array'});
+    workbook = XLSX.read(data, { type: 'array'});
     
-    const firstSheet = workbook.Sheets[workbook.SheetNames[0]]
-    excelData.value = XLSX.utils.sheet_to_json(firstSheet, {header: 1})
-    console.log(excelData.value)
+    sheetNames.value = workbook.SheetNames
+    selectedSheet.value = workbook.SheetNames[0];
+    loadSheetData();
   }
-  reader.readAsArrayBuffer(file);
+  reader.readAsArrayBuffer(file); 
 }
 
+const loadSheetData = () => {
+  const sheet = workbook.Sheets[selectedSheet.value];
+  const sheetData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+  
+  // Cargar solo las primeras 50 filas para evitar que la computadora se bloquee
+  excelData.value = sheetData.slice(0, 50);
+};
 
 </script>
 
 <template>
   <div>
-    <h1>Put Archive please</h1>
-    <input type="file" @change="handleFileUpload">
-    <div v-if="excelData.length">
-      <h3>Datos de Excel:</h3>
-      <table>
-        <tr v-for="(row, rowIndex) in excelData" :key="rowIndex">
-          <td v-for="(cell, cellIndex) in row" :key="cellIndex">{{ cell }}</td>
-        </tr>
-      </table>
-    </div>
+    <h1>Put Archive Please</h1>
+    <ChooseSheet :sheetNames="sheetNames" v-if="sheetNames.length > 0"/>
+    <input type="file" @change="handleFileUpload" v-else>
   </div>
 </template>
 
---
+++ src/components/ChooseSheet.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+const props = defineProps(['sheetNames'])
+const scrollContainer = ref(null);
+
+onMounted(() => {
+  const container = scrollContainer.value;
+
+  container.addEventListener('wheel', (e) => {
+    e.preventDefault();
+    container.scrollLeft += e.deltaY; // Convierte el desplazamiento vertical en desplazamiento horizontal
+  });
+});
+</script>
+<template>
+    <div v-if="props.sheetNames.length" >
+        <h3>Datos de Excel:</h3>
+        <main ref="scrollContainer">
+            <p v-for="(item, index) in sheetNames" :key="index">{{ item }}</p>
+        </main>
+    </div>
+</template>
+<style scoped>
+div{
+    height: 40vh;
+    width: 80vw;
+    background-color: #ebddddb9;
+    box-shadow: 0 0 10px #000000;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    overflow: hidden;
+}
+h3{
+    width: 100%;
+    margin: 0;
+    height: min-content;
+    color: rgba(27, 143, 27, 0.705);
+}
+main{
+    display: flex;
+    align-items: center;
+    width: 80vw;
+    height: 30vh;
+    overflow-x: scroll ;
+}
+p{
+    background-color: #242424;
+    box-shadow: 5px 5px 10px #0000009f;
+    height: 20vh;
+    flex: 1 1 auto;
+    min-width: 15vw;
+    max-width: 15vw;
+    margin: 0 1vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all ease .3s;
+    cursor: pointer;
+}
+p:hover{
+    background-color: rgba(27, 143, 27, 0.705) ;
+    border-radius: 8px;
+    color: #000000;
+}
+</style>
